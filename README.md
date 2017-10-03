3252350e46e7b6445e0cdb1bbc4a72bf3ef926d8
Alex M <oblivionpick@gmail.com>
2017-10-03 13:41:11 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+#PHP Uppgift med databaser
+Utgå från databasen klubben (klubben.sql)
+Använd den för att sedan skapa en webbplats där man kan ta bort/lägga till medlemmar eller sektioner samt lägga till/ta bort medlemmar i sektioner.
---
+++ addForm.php
@@ -0,0 +1,7 @@
+<form method="post">
+<table>
+	<tr><td>namn:</td><td><input type="text" name="namn"></td></tr>
+	<tr><td>telefon:</td><td><input type="text" name="tel"></td></tr>
+	<tr><td colspan="2"><input type="submit" name="submit" value="lägg till"></td></tr>
+</table>
+</form> 
